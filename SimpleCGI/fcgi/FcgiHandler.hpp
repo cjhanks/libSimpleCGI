7c8278618ad2b5798ac2f0ec6326147f3f67a9a8
Zeek Oni <web@cjhanks.name>
2017-06-14 15:08:43 -0700

    Remove unneeded file.

--- SimpleCGI/fcgi/FcgiHandler.hpp
+++ SimpleCGI/fcgi/FcgiHandler.hpp
@@ -1,27 +1,10 @@
 #ifndef __FCGI_HANDLER_HPP
 #define __FCGI_HANDLER_HPP
 
-#include <cstring>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <vector>
-
-#include "FcgiProtocol.hpp"
-#include "FcgiHttp.hpp"
-#include "FcgiStream.hpp"
-#include "FcgiMimetype.hpp"
-#include "SimpleCGI/common/Logging.hpp"
 #include "bits/FcgiHandler.hpp"
 
-
-namespace fcgi {
-class HttpRequest;
-class MasterServer;
-class LogicalApplicationSocket;
-
-
-
-} // ns fcgi
+#include "FcgiResponse.hpp"
+#include "FcgiRequest.hpp"
+#include "FcgiStream.hpp"
 
 #endif //__FCGI_HANDLER_HPP
